e8f19ede9abbab927753b7b62e6bbf8881265e73
Samuel Factor <sfxfactor@gmail.com>
2015-09-11 14:41:12 -0400

    added java and python files

---
+++ ackermann.java
@@ -0,0 +1,28 @@
+public class ackermann
+    public static void main(String[] args){
+
+        int[] acks=int[99999,99999]
+        for (m=0,m<6,m++){
+            for (n=0,n<6,n++){
+                acks = ack(m,n,acks)
+                System.out.println("Ack("+m+","+n+")="+acks[m,n])
+            }
+        }
+    }
+
+    public static int[] ack(int m,int n, int[] anot){
+
+        if anot[m,n]>0
+            return anot
+        else if m==0
+            anot[m,n]=n+1
+        else if n==0{
+            anot=ack(m-1,1,anot)
+            anot[m,n]=anot[m-1,1]}
+        else{
+            anot=ack(m,n-1,anot)
+            anot=ack(m-1,anot[m,n-1],anot)
+            anot[m,n]=anot[m-1,anot[m,n-1]]}
+        return anot
+    }
+
---
+++ ackermann.py
@@ -0,0 +1,26 @@
+#prints out the ackerman function numbers
+
+import time
+import numpy as np
+
+def ack(m,n,anot):
+    if anot[m,n]>=0:
+        return anot
+    elif m==0:
+        anot[m,n]=n+1
+    elif n==0:
+        anot=ack(m-1,1,anot)
+        anot[m,n]=anot[m-1,1]
+    else:
+        anot=ack(m,n-1,anot)
+        anot=ack(m-1,anot[m,n-1],anot)
+        anot[m,n]=anot[m-1,anot[m,n-1]]
+    return anot
+
+acks=np.zeros((9999,9999))-1
+for m in range(0,5):
+    for n in range(0,5):
+        ts=time.clock()
+        acks=ack(m,n,acks)
+        te=time.clock()
+        print "Ack({m},{n})={a} took {t}seconds".format(m=m,n=n,a=acks[m,n],t=(te-ts))
